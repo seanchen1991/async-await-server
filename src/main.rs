d010d5d82246aa55ffaf8bda3b101c6f41fc9d2a
Sean <seanchen11235@gmail.com>
2018-11-30 01:40:57 -0800

    Making progress

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #![feature(async_await, await_macro, futures_api)]
 
+use std::io;
+use std::fs::File;
 use std::io::prelude::*;
 
 use rand::Rng;
 
 use futures::StreamExt;
 use futures::executor::{self, ThreadPool};
-use futures::io::AsyncWriteExt;
+use futures::io::{AsyncReadExt, AsyncWriteExt};
 use futures::task::{SpawnExt};
 
 use romio::{TcpListener, TcpStream};
@@ -33,43 +35,57 @@ fn main() -> io::Result<()> {
 }
 
 async fn handle_request(mut stream: TcpStream) -> io::Result<()> {
-    let mut buffer = [0; 512];
-    await!(stream.read(&mut buffer)?);
+    let mut buffer = [0_u8; 512];
+    await!(stream.read(&mut buffer))?;
 
     let get_req = b"GET /";
     let d20_req = b"GET /d20";
     let post_req = b"POST /save";
 
     if buffer.starts_with(d20_req) {
-        await!(get_d20(stream)?);
-    } else if buffer.starts(get_req) {
-        await!(get_file(stream, &mut buffer)?);
-    } else if buffer.starts_with(post_req) {
-        await!(post_save(stream, &mut buffer)?);
+        await!(get_d20(stream))?;
+    // } else if buffer.starts(get_req) {
+    //     await!(get_file(stream, &mut buffer))?;
+    // } else if buffer.starts_with(post_req) {
+    //     await!(post_save(stream, &mut buffer))?;
     } else {
-        await!(get_404(stream)?);
+        await!(get_404(stream))?;
     }
 
     Ok(())
 }
 
-async fn get_404(stream: TcpStream) -> io::Result<()> {
-    let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-    let mut file = File::open("404.html")?;
-    let mut contents = Stream::new();
-    
-    file.read_to_stream(&mut contents)?;
+async fn get_d20(stream: TcpStream) -> io::Result<()> {
+    let status_line = &"HTTP/1.1 200 OK\r\n\r\n";
+    let contents = rand::thread_rng().gen_range(1, 21).to_string();
 
-    await!(send_response(stream, status_line, &contents)?);
+    await!(send_response(stream, status_line, contents))?;
 
     Ok(())
 }
 
-async fn send_response(mut stream: TcpStream, status_line: &str, contents: &str) -> io::Result<()> {
+async fn get_404(stream: TcpStream) -> io::Result<()> {
+    let status_line = &"HTTP/1.1 404 NOT FOUND\r\n\r\n";
+    let mut file = File::open("404.html")?;
+    let mut contents = String::new();
+    
+    file.read_to_string(&mut contents)?;
+
+    await!(send_response(stream, status_line, contents))?;
+
+    Ok(())
+}
+
+async fn get_file(stream: TcpStream, buffer: &mut [u8]) -> io::Result<()> {
+    
+
+    Ok(())
+}
+
+async fn send_response(mut stream: TcpStream, status_line: &str, contents: String) -> io::Result<()> {
     let response = format!("{}{}", status_line, contents);
 
-    await!(stream.write(response.as_bytes())?);
-    await!(stream.flush()?);
+    await!(stream.write_all(response.as_bytes()))?;
 
     Ok(())
 }
